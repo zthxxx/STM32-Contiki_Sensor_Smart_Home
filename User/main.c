d7b70a4d56256edf6448b0cec38305c225dfe568
tian <zth_9451@qq.com>
2016-04-30 12:10:11 +0800

    1) Add function condikional compile.

--- User/main.c
+++ User/main.c
@@ -27,19 +27,29 @@
 #include <clock.h>
 #include "contiki_delay.h"
 
+#define __WIFI_MODULE_ON__
+#define __OLED_MODULE_ON__
 
 PROCESS(red_blink_process, "Red Blink");
 PROCESS(green_blink_process, "Green Blink");
+PROCESS(wifi_send_test_process, "Wifi module send data test");
 PROCESS(IWDG_Feed_process, "Timing to feed dog");
-AUTOSTART_PROCESSES(&IWDG_Feed_process);
+AUTOSTART_PROCESSES(&etimer_process,&IWDG_Feed_process);
 
 void BSP_Config(void)
 {
     /* 初始化 */
     clock_init();
     USART1_Config(115200);
-    WiFi_Config();                       //初始化WiFi模块使用的接口和外设
-    OLED_Init();
+    
+#ifdef __WIFI_MODULE_ON__
+    WiFi_Config(); //初始化WiFi模块使用的接口和外设
+#endif
+    
+#ifdef __OLED_MODULE_ON__
+    OLED_Init(); //初始化OLED模块使用的接口和外设
+#endif                
+    
 }
 
 
@@ -47,19 +57,29 @@ int main(void)
 {	
     BSP_Config();    
     
-    OLED_ShowString(0,0,"SPI OLED");
-	OLED_ShowString(0,32,"Start OK!");
-	OLED_Refresh_Gram();//更新显示
+#ifdef __OLED_MODULE_ON__
+    {
+        OLED_ShowString(0,0,"SPI OLED");
+        OLED_ShowString(0,32,"Start OK!");
+        OLED_Refresh_Gram();//更新显示
+    }
+#endif 
+
+#ifdef __WIFI_MODULE_ON__    
     ESP8266_STA_TCP_Client();
+#endif
     
     IWDG_Start(2);  //wifi模块透传之后开启看门狗
     
     process_init();
-    process_start(&etimer_process,NULL);
     autostart_start(autostart_processes);
     process_start(&red_blink_process,NULL);
     process_start(&green_blink_process,NULL);
     
+#ifdef __WIFI_MODULE_ON__     
+    process_start(&wifi_send_test_process,NULL);
+#endif
+
     while (1)
     {
         do
@@ -69,7 +89,7 @@ int main(void)
 }
 
 
-
+/*******************PROCESS************************/
 
 
 PROCESS_THREAD(red_blink_process, ev, data)
@@ -78,18 +98,10 @@ PROCESS_THREAD(red_blink_process, ev, data)
     PROCESS_BEGIN();
     while(1)
     {
-        
         Contiki_etimer_DelayMS(500);
-    
         GPIO_SetBits(GPIOA, GPIO_Pin_8);
-        UART2_SendBuff = " asdfafasdf";
-        UART2_DMA_Send_Data(UART2_SendBuff, 11);
-        
         Contiki_etimer_DelayMS(500);
-        
         GPIO_ResetBits(GPIOA, GPIO_Pin_8);
-        UART2_SendBuff = " 21398416";
-        UART2_DMA_Send_Data(UART2_SendBuff, 11);
     }
     PROCESS_END();
 }
@@ -104,6 +116,24 @@ PROCESS_THREAD(green_blink_process, ev, data)
         GPIO_SetBits(GPIOD, GPIO_Pin_2);
         Contiki_etimer_DelayMS(200);
         GPIO_ResetBits(GPIOD, GPIO_Pin_2);
+    }
+    PROCESS_END();
+}
+
+PROCESS_THREAD(wifi_send_test_process, ev, data)
+{
+    static struct etimer et;
+    PROCESS_BEGIN();
+    while(1)
+    {
+        
+        Contiki_etimer_DelayMS(500);
+        UART2_SendBuff = " asdfafasdf";
+        UART2_DMA_Send_Data(UART2_SendBuff, 11);
+        
+        Contiki_etimer_DelayMS(500);        
+        UART2_SendBuff = " 21398416hy";
+        UART2_DMA_Send_Data(UART2_SendBuff, 11);
     }
     PROCESS_END();
 }
